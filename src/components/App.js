262e18dc7d8ee8086dc95dd74cab0d32584b1c9c
Parth Thakral <parth.thakral72@gmail.com>
2022-01-30 01:56:40 +0530

    Changing the state from Array to object

--- src/components/App.js
+++ src/components/App.js
@@ -22,8 +22,8 @@ class App extends React.Component {
   render(){
 
             
-      const movies=this.props.store.getState();
-      console.log('RENDER');
+      const { list }=this.props.store.getState();  //{list:[], favouries:[]}
+      console.log('RENDER',this.props.store.getState());
       return (
         <div className="App">
           <Navbar />
@@ -34,7 +34,7 @@ class App extends React.Component {
               </div>
 
               <div className="list">
-                  {movies.map((movie,index) => (
+                  {list.map((movie,index) => (
                     <MovieCard movie={movie} key={`movies-${index}`}/>
                   ))}
               </div>
