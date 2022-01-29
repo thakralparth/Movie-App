8bd5fce8b7253249fff64171500b1f5cbf28cd6c
Parth Thakral <parth.thakral72@gmail.com>
2022-01-29 23:53:01 +0530

    Dispatching actions

--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ import App from './components/App';
 import movies from './reducers';
 
 
-const store = createStore(movies);
+const store = createStore(movies);  // movies is a reducer -> pure function
 console.log('store',store);
-console.log('STATE',store.getState());
+console.log('BEFORE STATE',store.getState());   //initial state is defined in reducer function
+
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies : [ {name :'Superman'}]
+})
+
+console.log('AFTER STATE',store.getState()); 
 
 ReactDOM.render(
   <React.StrictMode>
