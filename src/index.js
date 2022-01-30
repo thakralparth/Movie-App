448212da90df1654b9368211664bc867f5656eeb
Parth Thakral <parth.thakral72@gmail.com>
2022-01-30 17:19:17 +0530

    Adding middleware, use currying function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
+//function logger(obj,next,action)
+//logger(obj)(next)(action)
+const logger = function({dispatch, getState}){
+  return function(next){
+    return function(action){
+      //middleware code
+      console.log('ACTION_TYPE = ',action.type);
+      next(action);  //to move to next middleware or to call dispatch again at end of last middleware
+    }
+  }
+}
 
-const store = createStore(rootReducer);  // movies is a reducer -> pure function
+
+const store = createStore(rootReducer,applyMiddleware(logger));  // movies is a reducer -> pure function
 console.log('store',store);
 // console.log('BEFORE STATE',store.getState());   //initial state is defined in reducer function
 
